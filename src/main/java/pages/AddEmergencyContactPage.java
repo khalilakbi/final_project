e193c9d3a9f9173835153565745d5518cd0f7693
gayaaris <rachidaifri90@gmail.com>
2025-01-13 20:06:23 -0500

    newchanges

--- src/main/java/pages/AddEmergencyContactPage.java
+++ src/main/java/pages/AddEmergencyContactPage.java
@@ -1,4 +1,67 @@
 package pages;
 
-public class AddEmergencyContact {
+import base.Base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class AddEmergencyContactPage extends Base {
+    public AddEmergencyContactPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+
+    @FindBy(xpath = "//span[text()='My Info']")
+    WebElement clickMyInfo;
+    @FindBy(xpath = "//a[text()='Emergency Contacts']")
+    WebElement clickEmergencyContacts;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/div/button")
+    WebElement clickAddButton;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[1]/div/div[2]/input")
+    WebElement inputEmergencyContactName;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[1]/div/div[2]/div/div[2]/input")
+    WebElement inputEmergencyContactRelationship;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div/div[1]/div/div[2]/input")
+    WebElement emergencyContactHomeTelephone;
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement clickOnSaveButton;
+
+
+    public void ClickOnMyInfo() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='My Info']")));
+        clickOnElement(clickMyInfo);
+    }
+
+    public void ClickOnEmergencyContacts() {
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//a[text()='Emergency Contacts']")));
+        clickOnElement(clickEmergencyContacts);
+    }
+
+    public void ClickOnAddButton() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/div/button")));
+        clickOnElement(clickAddButton);
+    }
+
+    public void enterEmergencyContactName(String emergencyContactName) {
+        wait.until(ExpectedConditions.visibilityOfAllElements(inputEmergencyContactName));
+        typeText(inputEmergencyContactName, emergencyContactName);
+    }
+
+    public void enterEmergencyContactRelationship(String emergencyContactRelationship) {
+        wait.until(ExpectedConditions.visibilityOfAllElements(inputEmergencyContactRelationship));
+        typeText(inputEmergencyContactRelationship, emergencyContactRelationship);
+    }
+
+    public void enterEmergencyContactHomeTelephone(String homeTelephone) {
+        wait.until(ExpectedConditions.visibilityOfAllElements(emergencyContactHomeTelephone));
+        typeText(emergencyContactHomeTelephone, homeTelephone);
+    }
+
+    public void clickSaveButton() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@type='submit']")));
+        clickOnElement(clickOnSaveButton);
+    }
+
 }
--- src/test/java/system/AddEmergencyContactTest.java
+++ src/test/java/system/AddEmergencyContactTest.java
@@ -1,4 +1,54 @@
 package system;
 
-public class AddEmergencyContactTest {
+import base.Base;
+import org.testng.annotations.Test;
+import pages.AddEmergencyContactPage;
+import pages.HomePage;
+
+import static java.lang.Thread.sleep;
+
+public class AddEmergencyContactTest extends Base {
+    HomePage homePage;
+    String validUsername = "Admin";
+    String validPassword = "admin123";
+    String emergencyContactName = "amine";
+    String emergencyContactRelationship = "spouse";
+    String emergencyContactHomeTelephone = "6105643781";
+
+    @Test
+    public void AddEmergencyContact() throws InterruptedException {
+        HomePage homePage = new HomePage();
+        AddEmergencyContactPage addEmergencyContactPage = new AddEmergencyContactPage();
+
+        logger.info("start test case valid login");
+        logger.info("open orange hrm application");
+        homePage = new HomePage();
+        logger.info("enter a valid username");
+        homePage.enterUserName(validUsername);
+        logger.info("enter a valid password");
+        homePage.enterPassword(validPassword);
+        logger.info("click on login button");
+        homePage.clickOnLogin();
+        logger.info("click on My Info");
+        addEmergencyContactPage.ClickOnMyInfo();
+        logger.info("click on Emergency Contacts");
+        addEmergencyContactPage.ClickOnEmergencyContacts();
+
+        logger.info("click on Add Button");
+        addEmergencyContactPage.ClickOnAddButton();
+
+        logger.info("enter emergency contact name");
+        addEmergencyContactPage.enterEmergencyContactName(emergencyContactName);
+
+        logger.info("enter emergency contact relationship");
+        addEmergencyContactPage.enterEmergencyContactRelationship(emergencyContactRelationship);
+        logger.info("enter emergency contact home telephone");
+        addEmergencyContactPage.enterEmergencyContactHomeTelephone(emergencyContactHomeTelephone);
+        logger.info("click on save button");
+        addEmergencyContactPage.clickSaveButton();
+
+
+    }
+
+
 }
